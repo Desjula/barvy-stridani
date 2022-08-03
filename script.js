36838e26a5e61300dc7c1bfcd6b5bc3d81499b60
Julie Desová <juliedesova@seznam.cz>
2022-08-03 11:31:12 +0200

    hotovo

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let svetlo = document.querySelector(".svetlo");
 
-let svestka = document.querySelector(".svestka");
+/*let svestka = document.querySelector(".svestka");
 svestka.addEventListener("click", function() {
     svetlo.style.backgroundColor = "plum"
 })
@@ -15,20 +15,50 @@ rajcatova.addEventListener("click", function() {
     svetlo.style.backgroundColor = "tomato"
 })
 
-/*setTimeout(function(){
-    svetlo.style.backgroundColor = "tomato", 1000})
+setTimeout(function(){
+    svetlo.style.backgroundColor = "plum"}, 3000)
 
 setTimeout(function(){
-svetlo.style.backgroundColor = "zelena", 2000})
+svetlo.style.backgroundColor = "zelena"}, 3000)
 
 setTimeout(function(){
-    svetlo.style.backgroundColor = "plum", 3000})*/
+    svetlo.style.backgroundColor = "tomato"}, 3000)
+
+ssetInterval(function(){
+    svetlo.style.backgroundColor = "tomato"}, 3000)
+
+etInterval(function(){
+svetlo.style.backgroundColor = "zelena"}, 3000)
 
 setInterval(function(){
-    svetlo.style.backgroundColor = "tomato", 1000})
+    svetlo.style.backgroundColor = "plum"}, 3000)*/
 
-setInterval(function(){
-svetlo.style.backgroundColor = "zelena", 1000})
+let barvy = ["tomato", "lime", "plum", "cyan", "yellow", "hotpink"]
+let pocetTiku = 0
+let casovac
 
-setInterval(function(){
-    svetlo.style.backgroundColor = "plum", 1000})
+let stop = document.querySelector(".stop");
+stop.addEventListener("click", function(){
+    clearInterval(casovac);
+})
+
+
+let start = document.querySelector(".start");
+start.addEventListener("click", function(){
+    casovac = setInterval(function(){
+    svetlo.style.backgroundColor = barvy[pocetTiku]
+
+if (pocetTiku > 4){
+    pocetTiku = 0
+} else {
+    pocetTiku += 1
+}
+},1000);
+})
+
+
+
+
+
+
+
